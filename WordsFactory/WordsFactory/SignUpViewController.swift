62008a0f909608eb3601c8336498036770a024cc
max911z <80741988+max911z@users.noreply.github.com>
2022-06-13 13:02:36 +0700

    Провёл первоначальную сборку проекта
    Поддержка версии >= 12.0, доступна только портретная ориентация
    Через SPM добавлены библиотеки SnapKit и Alamofire
    Создана папка Fonts

---
+++ WordsFactory/WordsFactory/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WordsFactory
+//
+//  Created by Максим Неверовский on 13.06.2022.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ WordsFactory/WordsFactory/Support/Extension.swift
@@ -0,0 +1,22 @@
+//
+//  Extension.swift
+//  WordsFactory
+//
+//  Created by Максим Неверовский on 13.06.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont{
+    
+    enum FontType: String{
+        case bold = "-Bold"
+        case regular = "-Regular"
+        case medium = "-Medium"
+    }
+    
+    static func rubik(_ type: FontType, size: CGFloat) -> UIFont{
+        return UIFont(name: "Rubik\(type.rawValue)", size: size)!
+    }
+}
