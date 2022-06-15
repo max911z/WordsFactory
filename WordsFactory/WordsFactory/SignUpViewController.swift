dab4c87cc3ed2687cff8c2aff12b5c8b1380e878
max911z <80741988+max911z@users.noreply.github.com>
2022-06-15 11:40:59 +0700

    Изменена логика отображения ответов
    Реализован запрос к серверу, добавлена структура result для отображения ответа
    Также исправлены баги, связанные со строковыми типами в swift

--- WordsFactory/WordsFactory/SignUpViewController.swift
+++ WordsFactory/WordsFactory/SignUpViewController.swift
@@ -41,6 +41,58 @@ class SignUpViewController: UIViewController, UITextFieldDelegate{
         SignUpButton.addShadow()
         BackButton.layer.cornerRadius = 28
         BackButton.addShadow()
+        var result: test?
+        let apiMethod = "http://193.38.50.175/itindr/api/mobile/v1/auth/register"
+        if MessageEmail.isHidden && MessagePassword.isHidden && MessageRepeatPassword.isHidden{
+            AF.request(apiMethod, method: .post, parameters: ["email":EmailTextField.text!,"password": PasswordTextField.text!], encoding: JSONEncoding.default).responseData{ [self] response in
+                switch response.result{
+                case .success (let data):
+                    print("Регистрация:", response.response!.statusCode)
+                    result = try? JSONDecoder().decode(test.self, from: data)
+                    
+                    if response.response!.statusCode == 200{
+                        let vc = InterestsViewController()
+                        vc.accessToken = result!.accessToken
+                        vc.refreshToken = result!.refreshToken
+                        navigationController?.pushViewController(vc, animated: true)
+                    }
+                    else if response.response!.statusCode == 400{
+                        
+                        let alert = UIAlertController(title: "Внимание", message: "Введите корректные данные", preferredStyle: .alert)
+                        let okeyButton = UIAlertAction(title: "Ok", style: .cancel )
+                        alert.addAction(okeyButton)
+                        
+                        self.present(alert, animated: true, completion: nil)
+                        
+                    }
+                    else if response.response!.statusCode == 409{
+                        let alert = UIAlertController(title: "Внимание", message: "Такой пользователь уже существует", preferredStyle: .alert)
+                        let okeyButton = UIAlertAction(title: "Ok", style: .cancel )
+                        alert.addAction(okeyButton)
+                        
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                    else if response.response!.statusCode == 500{
+                        let alert = UIAlertController(title: "Упс...", message: "Что-то пошло не так", preferredStyle: .alert)
+                        let okeyButton = UIAlertAction(title: "Ok", style: .cancel )
+                        alert.addAction(okeyButton)
+                        
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                    
+                case .failure (let error):
+                    print(error)
+                    let alert = UIAlertController(title: "Внимание", message: "Проверьте соединение с сервером", preferredStyle: .alert)
+                    let okeyButton = UIAlertAction(title: "Ok", style: .cancel )
+                    alert.addAction(okeyButton)
+                    
+                    self.present(alert, animated: true, completion: nil)
+                }
+                
+                
+            }
+        }
+        
     }
     
     
