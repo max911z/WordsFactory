c05fe37ca7116869067b18b2dc48a417dddf8e0a
max911z <80741988+max911z@users.noreply.github.com>
2022-06-16 10:44:57 +0700

    Реализовал экран для "Testing"
    Необходимо решить проблему создание ViewModel, чтобы можно было без проблем интегрировать код из Playground в XCode проект

---
+++ WordsFactory/WordsFactory/TestingViewController.swift
@@ -0,0 +1,56 @@
+//
+//  TestingViewController.swift
+//  WordsFactory
+//
+//  Created by Максим Неверовский on 16.06.2022.
+//
+
+import UIKit
+
+class TestingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    import XCTest
+    @testable import CodeWarsTesting
+
+    class CodeWarsUnitTests: XCTestCase {
+        var foldArray = CodeWarsTask()
+        
+        func testWhenInputIsEmptyArray() {
+            let array = [] as [Int]
+            let result = foldArray.foldArray(arr: array)
+            XCTAssertEqual(result, [])
+        }
+        
+        func testWhenInputIsNegativeAndPositiveNumbers() {
+            let array = [-9, 10, -100, -234, 34, 5] as [Int]
+            let result = foldArray.foldArray(arr: array)
+            XCTAssertEqual( result, [-4, 44, -334])
+        }
+        
+        func testWhenInputIsTimesMoreLengthArray() {
+            let array = [1, 2, 3, 4, 5] as [Int]
+            let result = foldArray.foldArray(arr: array, times: 10)
+            XCTAssertEqual( result, [15])
+        }
+        
+        func testWhenInputIsTimesEqualZero() {
+            let array = [1, 2, 3, 4, 5] as [Int]
+            let result = foldArray.foldArray(arr: array, times: 0)
+            XCTAssertEqual( result, [1, 2, 3, 4, 5])
+        }
+        
+        func testWhenInputArrayModified() {
+            let array = [1, 2, 3, 4, 5] as [Int]
+            _ = foldArray.foldArray(arr: array, times: 10)
+            XCTAssertEqual( array, [1, 2, 3, 4, 5])
+        }
+        
+    }
+
+}
