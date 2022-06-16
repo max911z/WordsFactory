0f1821f4810671c70a44a6b79ed03bbfed10bed8
max911z <80741988+max911z@users.noreply.github.com>
2022-06-16 11:12:09 +0700

    Создал экран для OnboardingScreens

---
+++ WordsFactory/WordsFactory/OnboardingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingViewController.swift
+//  WordsFactory
+//
+//  Created by Максим Неверовский on 16.06.2022.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
