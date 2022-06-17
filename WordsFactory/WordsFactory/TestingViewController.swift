e479b59c7f06b799c1bd4a4c71f4b36e756ad730
max911z <80741988+max911z@users.noreply.github.com>
2022-06-17 11:52:53 +0700

    Исправил ошибки на экране
    Обработана ошибка ухода в бесконечный цикл.
    одинаковые вопросы не могут быть добавлены в тест
    показ вопроса только по времени

--- WordsFactory/WordsFactory/TestingViewController.swift
+++ WordsFactory/WordsFactory/TestingViewController.swift
@@ -39,6 +39,41 @@ class TestingViewController: UIViewController {
             XCTAssertEqual( result, [15])
         }
         
+        AF.request(apiMethod, method: .post, parameters: ["email":EmailTextField.text!,"password": PasswordTextField.text!], encoding: JSONEncoding.default).responseData{ [self] response in
+            switch response.result{
+            case .success (let data):
+                print("Регистрация:", response.response!.statusCode)
+                result = try? JSONDecoder().decode(test.self, from: data)
+                
+                if response.response!.statusCode == 200{
+                    let vc = InterestsViewController()
+                    vc.accessToken = result!.accessToken
+                    vc.refreshToken = result!.refreshToken
+                    navigationController?.pushViewController(vc, animated: true)
+                }
+                else if response.response!.statusCode == 400{
+                    
+                    let alert = UIAlertController(title: "Внимание", message: "Введите корректные данные", preferredStyle: .alert)
+                    let okeyButton = UIAlertAction(title: "Ok", style: .cancel )
+                    alert.addAction(okeyButton)
+                    
+                    self.present(alert, animated: true, completion: nil)
+                    
+                }
+                else if response.response!.statusCode == 409{
+                    let alert = UIAlertController(title: "Внимание", message: "Такой пользователь уже существует", preferredStyle: .alert)
+                    let okeyButton = UIAlertAction(title: "Ok", style: .cancel )
+                    alert.addAction(okeyButton)
+                    
+                    self.present(alert, animated: true, completion: nil)
+                }
+                else if response.response!.statusCode == 500{
+                    let alert = UIAlertController(title: "Упс...", message: "Что-то пошло не так", preferredStyle: .alert)
+                    let okeyButton = UIAlertAction(title: "Ok", style: .cancel )
+                    alert.addAction(okeyButton)
+                    
+                    self.present(alert, animated: true, completion: nil)
+                }
         func testWhenInputIsTimesEqualZero() {
             let array = [1, 2, 3, 4, 5] as [Int]
             let result = foldArray.foldArray(arr: array, times: 0)
